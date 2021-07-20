a7a862fb9b133e858a1c9e1c594d514dcc38ef40
wg-d <wonderdream.dev@outlook.com>
2021-07-20 08:20:29 -0700

    change the details of product1

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,19 +8,19 @@ export interface Product {
   export const products = [
     {
       id: 1,
-      name: 'Phone XL',
+      name: 'Jack',
       price: 799,
       description: 'A large phone with one of the best screens'
     },
     {
       id: 2,
-      name: 'Phone Mini',
+      name: 'Harry',
       price: 699,
       description: 'A great phone with one of the best cameras'
     },
     {
       id: 3,
-      name: 'Phone Standard',
+      name: 'Susan',
       price: 299,
       description: ''
     }
